fd4cf854328bd55a719e6937b38f2543415b4774
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Air.local>
2019-06-16 18:23:57 -0300

    fix/ store/index.js.

--- src/store/index.js
+++ src/store/index.js
@@ -4,18 +4,15 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './ducks';
 import sagas from './sagas';
 
-/* ------------- Redux Configuration ------------- */
 const middlewares = [];
 
-/* ------------- Saga Middleware ------------- */
 const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
+
 const sagaMiddleware = createSagaMiddleware({
   sagaMonitor,
 });
 
 middlewares.push(sagaMiddleware);
-
-/* ------------- Assemble Middleware ------------- */
 
 const composer = __DEV__
   ? compose(
@@ -24,10 +21,8 @@ const composer = __DEV__
     )
   : compose(applyMiddleware(...middlewares));
 
-// create the store
 const store = createStore(reducers, composer);
 
-// kick off root saga
 sagaMiddleware.run(sagas);
 
 export default store;
